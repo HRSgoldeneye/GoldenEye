1edfca1bf627b022f38c48fa4ed6a773cc1b09d0
Yohan Yang <yohany@hawaii.edu>
2017-12-12 16:49:20 -1000

    admin_pg

---
+++ resources/views/pages/admin.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.master')
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-4">Admin Page</h1>
+        <p></p>
+        <p><a class="btn btn-primary btn-lg" href="https://github.com/HACC17/GoldenEye" role="button">GitHub Repo <i class="fa fa-github-square"></i></a></p>
+
+    </div>
+</div>
+@section ('content')
+    ------
+    Admin Page
+    -------
+
+@endsection
