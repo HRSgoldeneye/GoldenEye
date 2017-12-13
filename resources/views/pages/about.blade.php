80494cbda5b0401e65ed08ce51503cb1bc42f664
Yohan Yang <yohany@hawaii.edu>
2017-12-12 16:45:28 -1000

    about_pg

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,8 +3,12 @@
 <div class="jumbotron">
     <div class="container">
         <h1 class="display-4">About Page</h1>
-        <p>The easy to use app that allows easy accessibilty for our local laws, the Hawaii Revised Statutes </p>
+        <p>
+		On the official Hawai’i Government Website, <a href="https://portal.ehawaii.gov/government/hawaii-legislature/hawaii-revised-statutes/">Hawaiʻi Revised Statutes</a>, over 20,000 sections, with no database and no way to search for a particular statute or section, the statutes are on a file server. This means that it's another obstacle to update or edit statutes. Currently, the statutes are officially updated once a year. With this slow process, there is no consistency and typos are common for some statutes. </p>
+
+		<p>The easy to use app that allows easy accessibilty for our local laws, the Hawaii Revised Statutes.</p>
         <p><a class="btn btn-primary btn-lg" href="https://github.com/HACC17/GoldenEye" role="button">GitHub Repo <i class="fa fa-github-square"></i></a></p>
+
     </div>
 </div>
 @section ('content')
